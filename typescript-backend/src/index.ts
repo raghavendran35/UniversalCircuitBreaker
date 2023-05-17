51fd0d9fd845384b49eadeecb5bd49f71f08a519
raghavendran35 <sraghavendran@berkeley.edu>
2023-05-16 19:32:50 -0700

    remove aws references in entrypoint

--- typescript-backend/src/index.ts
+++ typescript-backend/src/index.ts
@@ -6,10 +6,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
-import * as AWSdynamoDB from "@aws-sdk/client-dynamodb";
-import * as AWSsqs from "@aws-sdk/client-sqs";
 import { coreRouter } from "./core_router";
-import { init } from "./implementations/circuit_breaker.implementations";
 
 
 //load in environment variables
@@ -39,27 +36,7 @@ app.use(cors());
 //can parse incoming requests with JSON payloads
 app.use(express.json());
 //take in optional path + callback function representing middleware function
-app.use("api/v1.0/cbass", coreRouter);
-
-// maybe can plugin/pass aws clients here
-const dynamoDBClient = new AWSdynamoDB.DynamoDB(
-    {
-        region: "us-east-2",
-        credentials:
-        { accessKeyId: process.env.AWS_ACCESS_KEY || "" ,
-          secretAccessKey: process.env.AWS_SECRET_KEY || ""
-        }
-    });
-const sqsClient = new AWSsqs.SQSClient(
-    {
-        region: "us-east-2",
-        credentials:
-        { accessKeyId: process.env.AWS_ACCESS_KEY || "" ,
-          secretAccessKey: process.env.AWS_SECRET_KEY || ""
-        }
-    });
-
-init(dynamoDBClient, sqsClient);
+app.use("api/v1.0/universalbreaker", coreRouter);
 
 /**
  * Server Activation
